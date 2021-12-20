9759454d47a3a1a5f04f85086b24e8ccfffc36dc
gabrielbadarau <badaraugabriel95@gmail.com>
2021-12-20 20:10:13 +0200

    back to home button

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Buttons from '../components/People/Buttons'
 import PeopleList from '../components/People/PeopleList'
+import { Link } from "react-router-dom";
 
 function People() {
     return (
         <div>
             <Buttons></Buttons>
             <PeopleList></PeopleList>
+            <button>
+                <Link to='/'>
+                    Introdu o nouă persoană
+                </Link>
+            </button>
         </div>
     )
 }
