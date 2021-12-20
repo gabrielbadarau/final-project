d5720bdedafb69b3b5c6fd60dc5c7287a72973f2
gabrielbadarau <badaraugabriel95@gmail.com>
2021-12-20 20:58:34 +0200

    updating Buttons component

--- src/components/People/Buttons.jsx
+++ src/components/People/Buttons.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
-function Buttons() {
-    return (
-        <div>
-            <p>Buttons Component</p>
-        </div>
-    )
+class Buttons extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>Sortează după:</p>
+                    <input type="checkbox" id="nameSort" value="nameSort"/>
+                    <label for="nameSort">nume</label>
+                    <input type="checkbox" id="salarySort" value="salarySort"/>
+                    <label for="salarySort">salariu</label>
+                </div>
+                <div>
+                    <p>Filtrează după salariu:</p>
+                    <input type="checkbox" id="1"/>
+                    <label for="1">sub 2500</label>
+                    <input type="checkbox" id="2"/>
+                    <label for="3">2500-4000</label>
+                    <input type="checkbox" id="3"/>
+                    <label for="3">peste 4000</label>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Buttons
